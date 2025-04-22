045dc847c334af4c68e47296d2c13e494154673a
przekwas <przekwas@gmail.com>
2025-04-22 14:38:35 -0500

    cleaning up my lab a bit

--- travel-agency-case-script.sh
+++ travel-agency-case-script.sh
@@ -2,35 +2,39 @@
 
 PS3="Please enter your city of choice: "
 select city in "Tokyo" "London" "Los Angeles" "Moscow" "Dubai" "Manchester" "New York" "Paris" "Bangalore" "Johannesburg" "Istanbul" "Milan" "Abu Dhabi" "Pune" "Nairobi" "Berlin" "Karachi"; do
+    
     case "$city" in
         "London" | "Manchester")
-            echo "That city is in the UK.";;
+            country="UK";;
         "Los Angeles" | "New York")
-            echo "That city is in the USA.";;
+            country="USA";;
         "Dubai" | "Abu Dhabi")
-            echo "That city is in the UAE.";;
+            country="UAE";;
         "Bangalore" | "Pune")
-            echo "That city is in India.";;
+            country="India";;
         "Tokyo")
-            echo "That city is in Japan.";;
+            country="Japan";;
         "Moscow")
-            echo "That city is in Russia.";;
+            country="Russia";;
         "Paris")
-            echo "That city is in France.";;    
+            country="France";;
         "Johannesburg")
-            echo "That city is in South Africa.";;
+            country="South Africa";;
         "Istanbul")
-            echo "That city is in Turkey.";;
+            country="Turkey";;
         "Milan")
-            echo "That city is in Italy.";;
+            country="Italy";;
         "Nairobi")
-            echo "That city is in Kenya.";;
+            country="Kenya";;
         "Berlin")
-            echo "That city is in Germany.";;
+            country="Germany";;
         "Karachi")
-            echo "That city is in Pakistan.";;
+            country="Pakistan";;
         *)
             echo "Idk how you chose a city without a country??";;
     esac
+
+    echo "That city is in $country."
+
     break
 done
